97b56c195a75967a6dda76b1c05d480ee43353c3
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-03-28 22:20:55 +0530

    verifed nodejs

--- JavaScript/demo.js
+++ JavaScript/demo.js
@@ -1,2 +1,11 @@
+// checkNode.js.js
 
-console.log("Hello, World!");
+console.log('Checking Node.js...');
+
+// Check Node.js version
+console.log('Node.js Version:', process.version);
+
+// Simple asynchronous task to check if async functionality is working
+setTimeout(() => {
+  console.log('Asynchronous task executed. Node.js is working correctly!');
+}, 1000);
