a9d8a5a9aac7a6cce06d9aa9861d1f470c21d869
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-04-03 14:51:23 +0530

    added js files

--- JavaScript/demo.js
+++ JavaScript/demo.js
@@ -1,11 +1,1 @@
-// checkNode.js.js
-
-console.log('Checking Node.js...');
-
-// Check Node.js version
-console.log('Node.js Version:', process.version);
-
-// Simple asynchronous task to check if async functionality is working
-setTimeout(() => {
-  console.log('Asynchronous task executed. Node.js is working correctly!');
-}, 1000);
+document.writeln("<h1>Hello World</h1>");
